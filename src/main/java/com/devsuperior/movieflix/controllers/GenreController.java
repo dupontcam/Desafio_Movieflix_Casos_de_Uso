7528d32bad78b8f13751531673f7d56cd6272f40
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-07-25 09:07:54 -0300

    GenreControllerIT Passed

--- src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -1,10 +1,13 @@
 package com.devsuperior.movieflix.controllers;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.services.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,17 @@ public class GenreController {
     @Autowired
     private GenreService genreService;
 
+    @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
     @GetMapping
     public ResponseEntity<List<GenreDTO>> findAll() {
         List<GenreDTO> list = genreService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GenreDTO> findById(@PathVariable Long id) {
+        GenreDTO dto = genreService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,13 +1,17 @@
 package com.devsuperior.movieflix.services;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GenreService {
@@ -19,5 +23,12 @@ public class GenreService {
     public List<GenreDTO> findAll(){
         List<Genre> list = genreRepository.findAll();
         return list.stream().map(x -> new GenreDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GenreDTO findById(Long id) {
+        Optional<Genre> obj = genreRepository.findById(id);
+        Genre entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new GenreDTO(entity);
     }
 }
